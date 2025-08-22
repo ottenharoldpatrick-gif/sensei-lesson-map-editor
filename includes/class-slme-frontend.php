4de480b162a6a7a27a62158d5451958ea8460127
Harold Otten <ottenharoldpatrick@gmail.com>
2025-08-22 18:18:58 +0200

    error 1818

--- includes/class-slme-frontend.php
+++ includes/class-slme-frontend.php
@@ -13,6 +13,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Frontend {
 
 	/**
+	 * Static init die door de loader wordt aangeroepen.
+	 * Houdt compatibel met includes/class-slme-loader.php: ::init()
+	 */
+	public static function init() : void {
+		new self();
+	}
+
+	/**
 	 * Constructor: registreert assets en shortcode.
 	 */
 	public function __construct() {
@@ -23,17 +31,17 @@ class Frontend {
 	/**
 	 * Laad frontend CSS (stabiele naam/locatie: assets/frontend.css).
 	 */
-	public function enqueue_assets() {
-		$ver = defined( 'SLME_VERSION' ) ? SLME_VERSION : '1.0.0';
+	public function enqueue_assets() : void {
+		$ver     = defined( 'SLME_VERSION' ) ? SLME_VERSION : '1.0.0';
 		$css_url = plugin_dir_url( dirname( __FILE__ ) ) . 'assets/frontend.css';
 		wp_enqueue_style( 'slme-frontend', $css_url, array(), $ver );
 	}
 
 	/**
 	 * Shortcode: [slme view="columns" course_id=""]
-	 * We renderen altijd via templates/grid-columns.php (stabiele naam/locatie).
+	 * We renderen via templates/grid-columns.php (stabiele naam/locatie).
 	 */
-	public function shortcode( $atts = array(), $content = '' ) {
+	public function shortcode( $atts = array(), $content = '' ) : string {
 		$atts = shortcode_atts(
 			array(
 				'view'      => 'columns',
@@ -48,19 +56,19 @@ class Frontend {
 			$course_id = get_the_ID();
 		}
 
-		// Veiligheid: render alleen op cursuspost of als expliciet course_id is gezet naar een cursus.
+		// Alleen renderen op een Sensei cursus of expliciete course_id die een cursus is.
 		if ( $course_id && 'course' !== get_post_type( $course_id ) ) {
 			return '';
 		}
 
-		$plugin_root   = dirname( __DIR__ ); // map van de plugin-root
+		$plugin_root   = dirname( __DIR__ ); // plugin-root map
 		$template_file = $plugin_root . '/templates/grid-columns.php';
 
 		if ( ! file_exists( $template_file ) ) {
 			return '<div class="slme-notice">SLME template niet gevonden (templates/grid-columns.php).</div>';
 		}
 
-		// Maak variabelen beschikbaar in de template indien nodig
+		// Variabele beschikbaar in template.
 		$slme_course_id = $course_id;
 
 		ob_start();
